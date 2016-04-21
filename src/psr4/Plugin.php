e20508ac86d6f0796bf5f7a587c780f10d11af7c
Alejandro Mostajo <amostajo@gmail.com>
2016-04-21 15:13:17 -0600

    Plugin improvements.
    Better hook definition system. Supports actions, filters, widgets and
    shortcodes.

--- src/psr4/Plugin.php
+++ src/psr4/Plugin.php
@@ -15,7 +15,7 @@ use Amostajo\LightweightMVC\Engine;
  * @author Alejandro Mostajo
  * @license MIT
  * @package Amostajo\WPPluginCore
- * @version 1.2
+ * @version 1.3
  */
 abstract class Plugin implements Plugable
 {
@@ -41,6 +41,34 @@ abstract class Plugin implements Plugable
 	protected $addons;
 
 	/**
+	 * List of Wordpress action hooks to add.
+	 * @var array
+	 * @since 1.3
+	 */
+	protected $actions;
+
+	/**
+	 * List of Wordpress filter hooks to add.
+	 * @var array
+	 * @since 1.3
+	 */
+	protected $filters;
+
+	/**
+	 * List of Wordpress shortcodes to add.
+	 * @var array
+	 * @since 1.3
+	 */
+	protected $shortcodes;
+
+	/**
+	 * List of Wordpress widgets to add.
+	 * @var array
+	 * @since 1.3
+	 */
+	protected $widgets;
+
+	/**
 	 * Main constructor
 	 * @since 1.0
 	 *
@@ -48,6 +76,10 @@ abstract class Plugin implements Plugable
 	 */
 	public function __construct( Config $config )
 	{
+		$this->actions = [];
+		$this->filters = [];
+		$this->shortcodes = [];
+		$this->widgets = [];
 		$this->config = $config;
 		$this->mvc = new Engine(
 			$this->config->get( 'paths.views' ),
@@ -85,6 +117,7 @@ abstract class Plugin implements Plugable
 	 * Calls to class or addon method.
 	 * Checks "addon_" prefix to search for addon methods.
 	 * @since 1.2
+	 * @since 1.3 Added MVC controller and views direct calls.
 	 *
 	 * @return mixed
 	 */
@@ -99,8 +132,43 @@ abstract class Plugin implements Plugable
 					break;
 				}
 			}
+		} else if ( preg_match( '/^\_c\_/', $method ) ) {
+			// Expected format
+			// _c_[return]_[mvccall]
+			// Sample - _c_void_ConfigController@local
+			$method_options = explode( '_', $method );
+			if ( count( $method_options ) != 4 ) return;
+			if ( $method_options[2] === 'return' ) {
+				return $this->mvc->action_args(
+					$method_options[3],
+					$this->override_args( $method_options[3], $args )
+				);
+			} else {
+				$this->mvc->call_args(
+					$method_options[3],
+					$this->override_args( $method_options[3], $args )
+				);
+			}
+		} else if ( preg_match( '/^\_v\_/', $method ) ) {
+			// Expected format
+			// _v_[return]_[mvccall]
+			// Sample - _v_void_View@test.view
+			$method_options = explode( '_', $method );
+			if ( count( $method_options ) != 4 ) return;
+			$view =  preg_replace( '/[vV]iew\@/', '', $method_options[3] );
+			if ( $method_options[2] === 'return' ) {
+				return $this->mvc->view->get(
+					$view,
+					$this->override_args( $method_options[3], $args )
+				);
+			} else {
+				$this->mvc->view->show(
+					$view,
+					$this->override_args( $method_options[3], $args )
+				);
+			}
 		} else {
-			call_user_func_array( [ $this, $method ], $args );
+			return call_user_func_array( [ $this, $method ], $args );
 		}
 	}
 
@@ -179,5 +247,201 @@ abstract class Plugin implements Plugable
 	public function on_admin()
 	{
 		// TODO custom code.
+	}
+
+	/**
+	 * Adds a Wordpress action hook.
+	 * @since 1.3
+	 *
+	 * @param string $hook 		    Wordpress hook name.
+	 * @param string $mvc_call      Lightweight MVC call. (i.e. 'Controller@method')
+	 * @param mixed  $priority      Execution priority or MVC params.
+	 * @param mixed  $accepted_args Accepted args or priority.
+	 * @param int    $args          Accepted args.
+	 */
+	public function add_action( $hook, $mvc_call, $priority = 10, $accepted_args = 1, $args = 1 )
+	{
+		$this->actions[] = $this->get_hook(
+			$hook,
+			$mvc_call,
+			$priority,
+			$accepted_args,
+			$args
+		);
+	}
+
+	/**
+	 * Adds a Wordpress filter hook.
+	 * @since 1.3
+	 *
+	 * @param string $hook 		    Wordpress hook name.
+	 * @param string $mvc_call      Lightweight MVC call. (i.e. 'Controller@method')
+	 * @param mixed  $priority      Execution priority or MVC params.
+	 * @param mixed  $accepted_args Accepted args or priority.
+	 * @param int    $args          Accepted args.
+	 */
+	public function add_filter( $hook, $mvc_call, $priority = 10, $accepted_args = 1, $args = 1 )
+	{
+		$this->filters[] = $this->get_hook(
+			$hook,
+			$mvc_call,
+			$priority,
+			$accepted_args,
+			$args
+		);
+	}
+
+	/**
+	 * Adds a Wordpress shortcode.
+	 * @since 1.3
+	 *
+	 * @param string $tag 	   Wordpress tag name.
+	 * @param string $mvc_call Lightweight MVC call. (i.e. 'Controller@method')
+	 */
+	public function add_shortcode( $tag, $mvc_call, $mvc_args = null )
+	{
+		$this->shortcodes[] = [
+			'tag'		=> $tag,
+			'mvc'		=> $mvc_call,
+			'mvc_args'	=> $mvc_args,
+		];
+	}
+
+	/**
+	 * Adds a Wordpress shortcode.
+	 * @since 1.3
+	 *
+	 * @param string $class Widget class name to add.
+	 */
+	public function add_widget( $class )
+	{
+		$this->widgets[] = $class;
+	}
+
+	/**
+	 * Adds hooks and filters into Wordpress core.
+	 * @since 1.3
+	 */
+	public function add_hooks()
+	{
+		if ( function_exists( 'add_action' )
+			&& function_exists( 'add_filter' )
+			&& function_exists( 'add_shortcode' )
+		) {
+			// Actions
+			foreach ( $this->actions as $action ) {
+				add_action(
+					$action['hook'],
+					[ &$this, $this->get_mapped_mvc_call( $action['mvc'] ) ],
+					$action['priority'],
+					$action['args']
+				);
+			}
+			// Filters
+			foreach ( $this->filters as $filter ) {
+				add_filter(
+					$filter['hook'],
+					[ &$this, $this->get_mapped_mvc_call( $filter['mvc'], true ) ],
+					$filter['priority'],
+					$filter['args']
+				);
+			}
+			// Filters
+			foreach ( $this->shortcodes as $shortcode ) {
+				add_shortcode(
+					$shortcode['tag'],
+					[ &$this, $this->get_mapped_mvc_call( $shortcode['mvc'], true ) ]
+				);
+			}
+			// Widgets
+			if ( count( $this->widgets ) > 0 ) {
+        		add_action( 'widgets_init', [ &$this, '_widgets' ], 1 );
+			}
+		}
+	}
+
+	/**
+	 * Registers added widgets into Wordpress.
+	 * @since 1.3
+	 */
+	public function _widgets()
+	{
+		foreach ( $this->widgets as $widget ) {
+			register_widget( $widget );
+		}
+	}
+
+	/**
+	 * Returns class method call mapped to a mvc engine method.
+	 * @since 1.3
+	 *
+	 * @return string
+	 */
+	private function get_mapped_mvc_call( $call, $return = false )
+	{
+		return ( preg_match( '/[vV]iew\@/', $call ) ? '_v_' : '_c_' )
+			. ( $return ? 'return_' : 'void_' )
+			. $call;
+	}
+
+	/**
+	 * Returns valid action filter item.
+	 * @since 1.3
+	 *
+	 * @param string $hook 		    Wordpress hook name.
+	 * @param string $mvc_call      Lightweight MVC call. (i.e. 'Controller@method')
+	 * @param mixed  $priority      Execution priority or MVC params.
+	 * @param mixed  $accepted_args Accepted args or priority.
+	 * @param int    $args          Accepted args.
+	 *
+	 * @return array
+	 */
+	private function get_hook( $hook, $mvc_call, $priority = 10, $accepted_args = 1, $args = 1 )
+	{
+		return [
+			'hook'		=> $hook,
+			'mvc'		=> $mvc_call,
+			'priority'	=> is_array( $priority ) ? $accepted_args : $priority,
+			'args'		=> is_array( $priority ) ? $args : $accepted_args,
+			'mvc_args'	=> is_array( $priority ) ? $priority : null,
+		];
+	}
+
+	/**
+	 * Override mvc arguments with those defined when adding an action or filter.
+	 * @since 1.3
+	 *
+	 * @param string $mvc_call Lightweight MVC call. (i.e. 'Controller@method')
+	 * @param array  $args     Current args for call.
+	 *
+	 * @return array
+	 */
+	private function override_args( $mvc_call, $args )
+	{
+		// Check on actions
+		for ( $i = count( $this->actions ) - 1; $i >= 0; --$i ) {
+			if ( ! empty( $this->actions[$i]['mvc_args'] )
+				&& $this->actions[$i]['mvc'] === $mvc_call
+			) {
+				return $this->actions[$i]['mvc_args'];
+			}
+		}
+		// Check on filters
+		for ( $i = count( $this->filters ) - 1; $i >= 0; --$i ) {
+			if ( ! empty( $this->filters[$i]['mvc_args'] )
+				&& $this->filters[$i]['mvc'] === $mvc_call
+			) {
+				return $this->filters[$i]['mvc_args'];
+			}
+		}
+		// Check on shortcodes
+		for ( $i = count( $this->shortcodes ) - 1; $i >= 0; --$i ) {
+			if ( ! empty( $this->shortcodes[$i]['mvc_args'] )
+				&& $this->shortcodes[$i]['mvc'] === $mvc_call
+			) {
+				return $this->shortcodes[$i]['mvc_args'];
+			}
+		}
+		return $args;
 	}
 }
