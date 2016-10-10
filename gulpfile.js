77baad0938a0a4fd5403d41ab4625834de91177d
Alejandro Mostajo <amostajo@gmail.com>
2016-10-10 14:40:54 -0600

    Assets and gulp.
    Assets folder structure and gulp integration.

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+/**
+ * Gulp
+ *
+ * @author Alejandro Mostajo <info@10quality.com>
+ * @package WPMVC
+ * @license MIT
+ * @version 1.0
+ */
+
+'use strict';
+
+// Prepare
+var fs = require('fs');
+var gulp = require('gulp');
+var wpmvc = require('gulp-wpmvc');
+
+// Load package JSON as config file.
+var config = JSON.parse(fs.readFileSync('./package.json'));
+
+// Init WPMVC default tasks.
+wpmvc(config);
+
+// --------------
+// START - CUSTOM TASKS
