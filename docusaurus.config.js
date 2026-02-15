d67e0a66aa9f5fcc28a5fd18185fb9a8ddcac8e3
Ale Mostajo <amostajo@gmail.com>
2026-02-14 19:29:04 -0600

    Docs fixes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   url: 'https://10quality.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: process.env.BASE_URL || '/',
+  baseUrl: process.env.BASE_URL || '/wpmvc/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`WordPress Themes and Plugins Framework`}
+      title={`WPMVC`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
